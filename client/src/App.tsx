98442bf49763622e984da8ace8d84f608272bc0c
krishnasaireddy425 <krishnasaireddy425@gmail.com>
2025-02-26 01:05:56 -0800

    new

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,27 @@
-import { Switch, Route } from "wouter";
+import { Router, Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+// Use "/" during development and "/PersonalPortfolio-4" in production
+const base = import.meta.env.DEV ? "/" : "/PersonalPortfolio-4";
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
+      {/* Conditional base path */}
+      <Router base={base}>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
       <Toaster />
     </QueryClientProvider>
   );
 }
+
 
 export default App;
